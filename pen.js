5ae1d36c335fe1739db857ed1e704bc74da6fd30
apple <apple@192.168.1.10>
2025-05-22 23:06:30 +0530

    nsejs lib added

--- pen.js
+++ pen.js
@@ -1,7 +1,15 @@
 const moment = require("moment");
-
+const { NSE, Extras } = require('nse-js');
+const nse = new NSE();
 const hours = (new Date()).getHours()
             const minutes = (new Date()).getMinutes()
 
 console.log("data flagstart", hours, minutes);
 console.log(moment("26-Jun-2024", "DD-MMM-YYYY").format('YYYY-MM-DD'))
+
+ const polo =async ()=>{
+    const data = await nse.optionChain('midcpnifty')
+    console.log("data here", data);
+}
+
+polo()
